59725365b3548b416710a04de2a040202f931fa8
DESKTOP-EJR3VAJ\Usuario <borjafruizdelgado@gmail.com>
2018-03-20 16:15:52 +0100

    Estructura de datos de estado implementada

---
+++ Lemmings/Lemmings/Estado.cpp
@@ -0,0 +1,20 @@
+#include "Estado.h"
+
+
+
+Estado::Estado()
+{
+	movingRight = true;
+	digging = false;
+	diggingDiagonal = false;
+	umbrella = false;
+	hasToBeRendered = false;
+	home = false;
+
+
+}
+
+
+Estado::~Estado()
+{
+}
---
+++ Lemmings/Lemmings/Estado.h
@@ -0,0 +1,17 @@
+#pragma once
+class Estado
+{
+public:
+	Estado();
+	~Estado();
+	bool movingRight;
+	bool digging;
+	bool diggingDiagonal;
+	bool umbrella;
+	bool hasToBeRendered;
+	bool home;
+private:
+	
+	
+};
+
