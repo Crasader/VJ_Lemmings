4a3da6f8c72d1d7745424a70bb054149e3afdff7
Isabel Codina <isakody@gmail.com>
2018-03-24 15:40:11 +0100

    getPosition Lemming
    Added all animations

--- Lemmings/Lemmings/Lemming.cpp
+++ Lemmings/Lemmings/Lemming.cpp
@@ -11,9 +11,6 @@
 #define FALL_STEP 4
 
 
-enum LemmingAnims {
-	WALKING_LEFT, WALKING_RIGHT, FALLING_LEFT, FALLING_RIGHT, DIGGER
-};
 
 
 Lemming::Lemming()
@@ -27,36 +24,17 @@ void Lemming::init(const glm::vec2 &initialPosition, ShaderProgram &shaderProgra
 	state = FALLING_RIGHT_STATE;
 
 	sprite = Sprite::createSprite(glm::ivec2(16, 16), glm::vec2(0.125, 0.03125), &spritesheet, &shaderProgram);
-	sprite->setNumberAnimations(5);
-		
-		sprite->setAnimationSpeed(DIGGER, speed);
-		for (int i = 0; i<8; i++)
-			sprite->addKeyframe(DIGGER, glm::vec2(float(i) / 8, 5.f / 32));
-		for (int i = 0; i<8; i++)
-			sprite->addKeyframe(DIGGER, glm::vec2(float(i) / 8, 6.f / 32));
+	sprite->setNumberAnimations(19);
 
-		sprite->setAnimationSpeed(WALKING_RIGHT, speed);
-		for(int i=0; i<8; i++)
-			sprite->addKeyframe(WALKING_RIGHT, glm::vec2(float(i) / 8, 0));
-		
-		sprite->setAnimationSpeed(WALKING_LEFT, speed);
-		for(int i=0; i<8; i++)
-			sprite->addKeyframe(WALKING_LEFT, glm::vec2(float(i) / 8, 1.f / 32));
+	setAnimations();
 
-		sprite->setAnimationSpeed(FALLING_RIGHT, speed);
-		for (int i = 0; i<4; i++)
-			sprite->addKeyframe(FALLING_RIGHT, glm::vec2(float(i) / 8, 2.f / 32));
-
-		sprite->setAnimationSpeed(FALLING_LEFT, speed);
-		for (int i = 4; i<8; i++)
-			sprite->addKeyframe(FALLING_LEFT, glm::vec2(float(i) / 8, 2.f / 32));
-		
 	sprite->changeAnimation(FALLING_RIGHT);
 	sprite->setPosition(initialPosition);
 }
 
-void Lemming::update(int deltaTime)
-{
+
+
+void Lemming::update(int deltaTime) {
 	int fall;
 
 	if(sprite->update(deltaTime) == 0)
@@ -65,9 +43,10 @@ void Lemming::update(int deltaTime)
 	switch(state)	{
 		case FALLING_LEFT_STATE:
 			fall = collisionFloor(2);
-			if(fall > 0)
-				sprite->position() += glm::vec2(0, fall);
-			else {
+			if (fall > 0) {		// more falling
+				move(0, fall);
+			}
+			else {				// no more falling
 				if (nextState == DIGGER_TRIGGERED) {
 					oldState = state;
 					state = DIGGER_TRIGGERED;
@@ -79,11 +58,13 @@ void Lemming::update(int deltaTime)
 			}
 			break;
 
+
 		case FALLING_RIGHT_STATE:
 			fall = collisionFloor(2);
-			if(fall > 0)
-				sprite->position() += glm::vec2(0, fall);
-			else {
+			if (fall > 0) {		// more falling
+				move(0, fall);
+			}
+			else {				// no more falling
 				if (nextState == DIGGER_TRIGGERED) {
 					oldState = state;
 					state = DIGGER_TRIGGERED;
@@ -95,39 +76,37 @@ void Lemming::update(int deltaTime)
 			}
 			break;
 
+
 		case WALKING_LEFT_STATE:
-			sprite->position() += glm::vec2(-1, -1);
-			if(collision()) {
-				sprite->position() -= glm::vec2(-1, -1);
+			move(-1, -1);
+			if(collision()) {	// wall
+				move(1, 1);
 				sprite->changeAnimation(WALKING_RIGHT);
 				state = WALKING_RIGHT_STATE;
 			}
-			else {
+			else {				// no wall
 				fall = collisionFloor(3);
-				if(fall > 0)
-					sprite->position() += glm::vec2(0, 1);
-				if(fall > 1)
-					sprite->position() += glm::vec2(0, 1);
-				if (fall > 2) {
+				if (fall < 3)
+					move(0, fall);
+				else {
 					sprite->changeAnimation(FALLING_LEFT);
 					state = FALLING_LEFT_STATE;
 				}
 			}
 			break;
 
+
 		case WALKING_RIGHT_STATE:
-			sprite->position() += glm::vec2(1, -1);
-			if(collision())
-			{
-				sprite->position() -= glm::vec2(1, -1);
+			move(1, -1);
+			if(collision()) {	// wall
+				move(-1, 1);
 				sprite->changeAnimation(WALKING_LEFT);
 				state = WALKING_LEFT_STATE;
 			}
-			else
-			{
+			else {				// no wall
 				fall = collisionFloor(3);
 				if (fall < 3) 
-					sprite->position() += glm::vec2(0, fall);
+					move(0, fall);
 				else {
 					sprite->changeAnimation(FALLING_RIGHT);
 					state = FALLING_RIGHT_STATE;
@@ -136,25 +115,27 @@ void Lemming::update(int deltaTime)
 			}
 			break;
 
+
 		case DIGGER_TRIGGERED:
 			fall = collisionFloor(2);
 			if (fall > 0) {
 				if (oldState == FALLING_LEFT_STATE) {
 					state = FALLING_LEFT_STATE;
-					sprite->position() += glm::vec2(0, 1);
+					move(0, 1);
 				}
 				else {
 					state = FALLING_RIGHT_STATE;
-					sprite->position() += glm::vec2(0, 1);
+					move(0, 1);
 				}
 				nextState = DIGGER_TRIGGERED;
 			}
 			else {
-				sprite->position() += glm::vec2(0, 1);
+				move(0, 1);
 				sprite->changeAnimation(DIGGER);
 				state = DIGGER_STATE;
 			}
 			break;
+
 
 		case DIGGER_STATE:
 			fall = collisionFloor(2);
@@ -167,10 +148,10 @@ void Lemming::update(int deltaTime)
 					sprite->changeAnimation(FALLING_RIGHT);
 					state = FALLING_RIGHT_STATE;
 				}
-				sprite->position() += glm::vec2(0, 0.125);
+				move(0, 0.125f);
 			}
 			else {
-				sprite->position() += glm::vec2(0, 0.125);
+				move(0, 0.125f);
 			}
 			break;
 
@@ -179,14 +160,16 @@ void Lemming::update(int deltaTime)
 	}
 }
 
-void Lemming::render()
-{
+void Lemming::render() {
 	sprite->render();
 }
 
-void Lemming::setMapMask(VariableTexture *mapMask)
-{
+void Lemming::setMapMask(VariableTexture *mapMask) {
 	mask = mapMask;
+}
+
+glm::vec2 Lemming::getPosition() {
+	return sprite->position();
 }
 
 int Lemming::collisionFloor(int maxFall)
@@ -218,6 +201,10 @@ bool Lemming::collision()
 	return true;
 }
 
+void Lemming::move(float x, float y) {
+	sprite->position() += glm::vec2(x, y);
+}
+
 void Lemming::changeState(int x) {
 	if (x == 1) {
 		oldState = state;
@@ -226,7 +213,123 @@ void Lemming::changeState(int x) {
 
 }
 
+void Lemming::setAnimations() {
 
+	/* Walking */
+		sprite->setAnimationSpeed(WALKING_RIGHT, speed);
+		for (int i = 0; i<8; i++)
+			sprite->addKeyframe(WALKING_RIGHT, glm::vec2(float(i) / 8, 0));
+
+		sprite->setAnimationSpeed(WALKING_LEFT, speed);
+		for (int i = 0; i<8; i++)
+			sprite->addKeyframe(WALKING_LEFT, glm::vec2(float(i) / 8, 1.f / 32));
+
+	/* Falling */
+		sprite->setAnimationSpeed(FALLING_RIGHT, speed);
+		for (int i = 0; i<4; i++)
+			sprite->addKeyframe(FALLING_RIGHT, glm::vec2(float(i) / 8, 2.f / 32));
+
+		sprite->setAnimationSpeed(FALLING_LEFT, speed);
+		for (int i = 4; i<8; i++)
+			sprite->addKeyframe(FALLING_LEFT, glm::vec2(float(i) / 8, 2.f / 32));
+
+	/* Blocking */
+		sprite->setAnimationSpeed(BLOCKER, speed);
+		for (int i = 0; i<8; i++)
+			sprite->addKeyframe(BLOCKER, glm::vec2(float(i) / 8, 3.f / 32));
+		for (int i = 0; i<8; i++)
+			sprite->addKeyframe(BLOCKER, glm::vec2(float(i) / 8, 4.f / 32));
+
+	/* Digging */
+		sprite->setAnimationSpeed(DIGGER, speed);
+		for (int i = 0; i<8; i++)
+			sprite->addKeyframe(DIGGER, glm::vec2(float(i) / 8, 5.f / 32));
+		for (int i = 0; i<8; i++)
+			sprite->addKeyframe(DIGGER, glm::vec2(float(i) / 8, 6.f / 32));
+
+	/* Bashing */
+		sprite->setAnimationSpeed(BASHER_RIGHT, speed);
+		for (int i = 0; i<8; i++)
+			sprite->addKeyframe(BASHER_RIGHT, glm::vec2(float(i) / 8, 7.f / 32));
+		for (int i = 0; i<8; i++)
+			sprite->addKeyframe(BASHER_RIGHT, glm::vec2(float(i) / 8, 8.f / 32));
+		for (int i = 0; i<8; i++)
+			sprite->addKeyframe(BASHER_RIGHT, glm::vec2(float(i) / 8, 9.f / 32));
+		for (int i = 0; i<8; i++)
+			sprite->addKeyframe(BASHER_RIGHT, glm::vec2(float(i) / 8, 10.f / 32));
+
+		sprite->setAnimationSpeed(BASHER_RIGHT, speed);
+		for (int i = 0; i<8; i++)
+			sprite->addKeyframe(BASHER_RIGHT, glm::vec2(float(i) / 8, 11.f / 32));
+		for (int i = 0; i<8; i++)
+			sprite->addKeyframe(BASHER_RIGHT, glm::vec2(float(i) / 8, 12.f / 32));
+		for (int i = 0; i<8; i++)
+			sprite->addKeyframe(BASHER_RIGHT, glm::vec2(float(i) / 8, 13.f / 32));
+		for (int i = 0; i<8; i++)
+			sprite->addKeyframe(BASHER_RIGHT, glm::vec2(float(i) / 8, 14.f / 32));
+
+	/* Climbing */
+		sprite->setAnimationSpeed(CLIMBER_RIGHT, speed);
+		for (int i = 0; i<8; i++)
+			sprite->addKeyframe(CLIMBER_RIGHT, glm::vec2(float(i) / 8, 15.f / 32));
+
+		sprite->setAnimationSpeed(CLIMBER_LEFT, speed);
+		for (int i = 0; i<8; i++)
+			sprite->addKeyframe(CLIMBER_LEFT, glm::vec2(float(i) / 8, 16.f / 32));
+
+	/* Climbing top */
+		sprite->setAnimationSpeed(CLIMBER_TOP_RIGHT, speed);
+		for (int i = 0; i<8; i++)
+			sprite->addKeyframe(CLIMBER_TOP_RIGHT, glm::vec2(float(i) / 8, 17.f / 32));
+
+		sprite->setAnimationSpeed(CLIMBER_TOP_LEFT, speed);
+		for (int i = 0; i<8; i++)
+			sprite->addKeyframe(CLIMBER_TOP_LEFT, glm::vec2(float(i) / 8, 18.f / 32));
+
+	/* Building */
+		sprite->setAnimationSpeed(BUILDER_RIGHT, speed);
+		for (int i = 0; i < 8; i++)
+			sprite->addKeyframe(BUILDER_RIGHT, glm::vec2(float(i) / 8, 19.f / 32));
+		for (int i = 0; i<8; i++)
+			sprite->addKeyframe(BUILDER_RIGHT, glm::vec2(float(i) / 8, 20.f / 32));
+
+		sprite->setAnimationSpeed(BUILDER_LEFT, speed);
+		for (int i = 0; i<8; i++)
+			sprite->addKeyframe(BUILDER_LEFT, glm::vec2(float(i) / 8, 21.f / 32));
+		for (int i = 0; i<8; i++)
+			sprite->addKeyframe(BUILDER_LEFT, glm::vec2(float(i) / 8, 22.f / 32));
+
+	/* Builder Stoping */
+		sprite->setAnimationSpeed(BUILDER_STOP_RIGHT, speed);
+		for (int i = 0; i<8; i++)
+			sprite->addKeyframe(BUILDER_STOP_RIGHT, glm::vec2(float(i) / 8, 23.f / 32));
+
+		sprite->setAnimationSpeed(BUILDER_STOP_LEFT, speed);
+		for (int i = 0; i<8; i++)
+			sprite->addKeyframe(BUILDER_STOP_LEFT, glm::vec2(float(i) / 8, 24.f / 32));
+
+	/* Exiting */
+		sprite->setAnimationSpeed(EXIT, speed);
+		for (int i = 0; i<8; i++)
+			sprite->addKeyframe(EXIT, glm::vec2(float(i) / 8, 25.f / 32));
+
+	/* Dying Explosion */
+		sprite->setAnimationSpeed(DIE_EXPLOSION, speed);
+		for (int i = 0; i<8; i++)
+			sprite->addKeyframe(DIE_EXPLOSION, glm::vec2(float(i) / 8, 26.f / 32));
+		for (int i = 0; i<8; i++)
+			sprite->addKeyframe(DIE_EXPLOSION, glm::vec2(float(i) / 8, 27.f / 32));
+
+	/* Dying Fall */
+		sprite->setAnimationSpeed(DIE_EXPLOSION, speed);
+		for (int i = 0; i<8; i++)
+			sprite->addKeyframe(DIE_EXPLOSION, glm::vec2(float(i) / 8, 28.f / 32));
+		for (int i = 0; i<8; i++)
+			sprite->addKeyframe(DIE_EXPLOSION, glm::vec2(float(i) / 8, 29.f / 32));
+
+
+	
+}
 
 
 
--- Lemmings/Lemmings/Lemming.h
+++ Lemmings/Lemmings/Lemming.h
@@ -17,6 +17,7 @@ class Lemming
 public:
 	Lemming();
 	void init(const glm::vec2 &initialPosition, ShaderProgram &shaderProgram, Texture &spritesheet,VariableTexture *mask);
+	void setAnimations();
 	void update(int deltaTime);
 	void render();
 	
@@ -24,19 +25,46 @@ public:
 	void changeState(int x);
 	void setMapMask(VariableTexture *mapMask);
 	
+	glm::vec2 getPosition();
+	
 private:
 	int collisionFloor(int maxFall);
 	bool collision();
 
+	void move(float x, float y);
+
 	
+
+public:
+	enum LemmingStatus {
+		ALIVE_STATUS,	/* Siguen vivos				*/
+		EXITED_STATUS,	/* Han finalizado el nivel	*/
+		DEAD_STATUS		/* Han muerto				*/
+	};
 	
 private:
-	enum LemmingState
-	{
-		WALKING_LEFT_STATE, WALKING_RIGHT_STATE, FALLING_LEFT_STATE, FALLING_RIGHT_STATE, DIGGER_STATE, DIGGER_TRIGGERED
+	enum LemmingState {
+		WALKING_LEFT_STATE, WALKING_RIGHT_STATE,
+		FALLING_LEFT_STATE, FALLING_RIGHT_STATE, 
+		DIGGER_STATE, DIGGER_TRIGGERED
 	};
 
-	LemmingState state, oldState, nextState;
+	enum LemmingAnims {		// 19 ANIMATIONS
+		WALKING_RIGHT, WALKING_LEFT,
+		FALLING_RIGHT, FALLING_LEFT,
+		BLOCKER,
+		DIGGER,
+		BASHER_RIGHT, BASHER_LEFT,
+		CLIMBER_RIGHT, CLIMBER_LEFT,
+		CLIMBER_TOP_RIGHT, CLIMBER_TOP_LEFT,
+		BUILDER_RIGHT, BUILDER_LEFT,
+		BUILDER_STOP_RIGHT, BUILDER_STOP_LEFT,
+		EXIT,
+		DIE_EXPLOSION,
+		DIE_FALL
+	};
+
+	LemmingState oldState, state, nextState;
 	//Texture spritesheet1;
 	Sprite *sprite;
 	VariableTexture *mask;
